1b1e736a31bee551281d8b2c5e16793822c3d96c
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2020-11-23 13:45:53 -0500

    added input event handler

--- src/App.js
+++ src/App.js
@@ -7,29 +7,42 @@ function App() {
     const APP_KEY = "561fab1d7ef32174e6725e98c7ff27c7";
 
     const [recipes, setRecipes] = useState([]);
+    const [search, setSearch] = useState("");
 
     useEffect(() => {
         getRecipes();
-    }, []);
+        console.log("Fetching");
+    }, [search]);
 
     const getRecipes = async () => {
         const response = await fetch(
-            `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+            `https://api.edamam.com/search?q=${search}&app_id=${APP_ID}&app_key=${APP_KEY}`
         );
         const data = await response.json();
         setRecipes(data.hits);
     };
 
+    const updateSearch = (e) => {
+        setSearch(e.target.value);
+        console.log(search);
+    };
+
     return (
         <div className="App">
             <form className="search-form">
-                <input type="text" className="search-bar" />
+                <input
+                    type="text"
+                    className="search-bar"
+                    value={search}
+                    onChange={updateSearch}
+                />
                 <button type="submit" className="search-button">
                     Search
                 </button>
             </form>
             {recipes.map((recipe) => (
                 <Recipe
+                    key={recipe.recipe.label}
                     title={recipe.recipe.label}
                     image={recipe.recipe.image}
                     calories={recipe.recipe.calories}
